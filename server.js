468ad005b4836a6c56b42d7ec74b130ad6ee6fcf
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-09-30 17:43:46 -0700

    Added app.use from express for urlencoded, site to have the path into the public folder where the html files are loaded, and json

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const path = require('path');
 const app = express();
+
+app.listen(PORT, function () {
+    console.log("Application listening on PORT" + PORT);
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
