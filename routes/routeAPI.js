fbcd7c2c042fa44a8a35bc4513d41ba25b58f8c5
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-09-30 18:32:15 -0700

    Able to display test, and post, however a duplicate is made with each push, as well as no receiving note when clicked

--- routes/routeAPI.js
+++ routes/routeAPI.js
@@ -7,3 +7,19 @@ function retrieveNotes() {
 
     return note;
 }
+
+module.exports = (app) => {
+    app.get("/api/notes", (req, res) => {
+        noteInfo = retrieveNotes();
+        res.json(noteInfo);
+        noteInfo.push(req.body);
+        res.json(true);
+    });
+
+    app.post("/api/notes", (req, res) => {
+        noteInfo.push(req.body);
+        fs.writeFileSync("./db/db.json", JSON.stringify(noteInfo));
+        res.json(true);
+    })
+
+};
